a7832458b032aabfa408418cb5f7ddb1893afc14
Jennifer Chinzi <jenrose87@gmail.com>
2020-09-21 16:50:24 -0700

    All class components converted to functional components

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import ToDo from './components/todo/todo.js';
+import Header from './components/header.js';
+
+
+function App() {
+
+    return (
+      <>
+        <Header />
+        <ToDo />
+      </>
+    );
+  
+}
+
+export default App;
